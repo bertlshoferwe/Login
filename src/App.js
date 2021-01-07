c09ada99b315dc52143de06ae94d39942f8c7ba2
bertlshoferwe <bertlshoferwe@yahoo.com>
2021-01-07 09:12:10 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from 'react'
+import LoginForm from './components/LoginForm'
+
 
 function App() {
+
+  const adminUser = {
+    email: 'admin@admin.com',
+    password: 'admin123'
+  }
+
+  const [ user, setUser]  = useState({ name:'' , email: '' });
+  const [ error, setError ] = useState('');
+
+  const Login = details => {
+    console.log( details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password) {
+
+      console.log('Logged in');
+
+      setUser({
+        name: details.name,
+        email: details.emails
+      });
+
+    }else{
+
+      console.log('Details did not matchs')
+      setError('Details did not matchs')
+
+    }
+  }
+
+  const Logout = () => {
+    console.log( 'Logout')
+
+    setUser({
+      name: '',
+      email: ''
+    })
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      { (user.email != '') ? (
+        <div className='welcome'>
+            <h2> Welcome, <span> { user.name } </span ></h2>
+            <button onClick={ () => Logout() }> Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} Error={error} />
+      )}
+
     </div>
   );
+
 }
 
 export default App;
